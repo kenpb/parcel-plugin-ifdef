9e22c7b71e6d46798d3ba10f4c9138145c9ab6c9
Junxiao Shi <git@mail1.yoursunny.com>
2020-06-08 23:06:36 -0400

    fix: filter environment variables
    fixes #2

--- js-asset.js
+++ js-asset.js
@@ -1,10 +1,11 @@
 const JSAsset = require('parcel-bundler/src/assets/JSAsset')
 const { parse } = require('ifdef-loader/preprocessor')
+const { filterEnvVariables } = require('./util')
 
 class JSAssetIFDEF extends JSAsset {
   async pretransform() {
     // run the loader on the source
-    this.contents = parse(this.contents, { ...filterVariables(process.env) })
+    this.contents = parse(this.contents, { ...filterEnvVariables(process.env) })
     // continue the normal flow
     return await super.pretransform()
   }
--- util.js
+++ util.js
@@ -1,2 +1,17 @@
-// workaround for: https://github.com/kenpb/parcel-plugin-ifdef/issues/2
-export const filterEnvVariables = (variables) => variables.filter(env => !/\(|\)/.test(env))
+/**
+ * Delete environment variables that are not valid JavaScript variable names.
+ * @param {Record<string, string>} variables
+ * @returns {Record<string, string>}
+ * @see https://github.com/kenpb/parcel-plugin-ifdef/issues/2
+ */
+function filterEnvVariables(variables) {
+  const result = {}
+  for (const [key, value] of Object.entries(variables)) {
+    if (/^[A-Za-z_$][A-Za-z_$0-9]*$/.test(key)) {
+      result[key] = value
+    }
+  }
+  return result
+}
+
+exports.filterEnvVariables = filterEnvVariables
